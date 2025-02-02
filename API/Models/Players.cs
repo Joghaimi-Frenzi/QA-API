5cf23a615cc5810f9ada087701d9e434ccd0a07c
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2025-02-02 20:26:53 +0300

    cc

--- API/Models/Players.cs
+++ API/Models/Players.cs
@@ -1,10 +1,15 @@
-﻿namespace API.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
 {
     public class Players
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]  // Ensures auto-increment
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Answers { get; set; }
+        public string Answers { get; set; }
         public bool isCorrect { get; set; }
     }
 }
